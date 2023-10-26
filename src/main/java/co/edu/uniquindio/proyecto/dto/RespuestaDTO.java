f8757f26600742e11b1774e0f7fc6f615c545426
SirJoao444 <144991198+SirJoao444@users.noreply.github.com>
2023-10-26 10:37:15 -0500

    Update RespuestaDTO.java

--- src/main/java/co/edu/uniquindio/proyecto/dto/RespuestaDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/RespuestaDTO.java
@@ -3,7 +3,7 @@ package co.edu.uniquindio.proyecto.dto;
 import co.edu.uniquindio.proyecto.modelo.entidades.Mensaje;
 
 import java.time.LocalDateTime;
-
+@requestbody
 public record RespuestaDTO(
         int codigoMensaje,
         String mensaje,
